4b6ef138fc269d60bb25bd8b0e0b1b61af69d06f
lengocan <lengocan2511@gmail.com>
2024-12-17 17:27:15 +0700

    done hero and list favorite item

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,18 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Hero from "@/components/home/Hero";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
-
+import { Suspense } from "react";
+import { Divide } from "lucide-react";
+import LoadingContainer from "@/components/global/LoadingContainer";
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <Suspense fallback={
+        <LoadingContainer />
+      }>
+
+      </Suspense>
       <FeaturedProducts />
     </>
 
--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import HeroCarousel from './HeroCarousel'
 
 function Hero() {
     return (
-        <div>Hero</div>
+        <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center pb-40'>
+            <div>
+                <h1 className='max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl'>
+                    We are changing the way people shop
+                </h1>
+                <p className='mt-8 max-w-xl text-lg leading-8 text-muted-foreground'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut possimus perferendis quod sapiente iste temporibus harum hic veritatis sequi provident, adipisci, sit necessitatibus. Est, deleniti?
+                </p>
+                <Button asChild size='lg' className='mt-10'>
+                    <Link href='/products'>Our products</Link>
+                </Button>
+            </div>
+            <div>
+                <HeroCarousel />
+            </div>
+        </section>
     )
 }
 
